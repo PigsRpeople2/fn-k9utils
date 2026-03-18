01f978e8211d7200ef11b16414b4118a83d74ed8
PigsRpeople2 <167029562+PigsRpeople2@users.noreply.github.com>
2026-03-19 04:13:54 +1100

    basic file structure

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+games { 'gta5' }
+lua54 'yes'
+
+author 'PigsRpeople2'
+description 'K9 utilities built for Faded Networks'
+
+shared_scripts {
+    'config/cfg_main.lua',
+    '@ox_lib/init.lua',
+}
+
+client_scripts {
+    'client/cl_*.lua' 
+}
+
+server_scripts {
+    'server/sv_*.lua'
+}
+-- asraye is gay lol
